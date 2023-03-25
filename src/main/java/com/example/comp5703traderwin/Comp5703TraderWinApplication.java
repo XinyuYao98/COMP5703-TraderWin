82e31aec2e1142761618f3a0e4f0cd4e849eedf8
Xinyu yao <xyao6818@uni.sydney.edu.au>
2023-03-25 13:54:25 +0800

    Initial commit

---
+++ src/main/java/com/example/comp5703traderwin/Comp5703TraderWinApplication.java
@@ -0,0 +1,13 @@
+package com.example.comp5703traderwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp5703TraderWinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp5703TraderWinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/comp5703traderwin/Comp5703TraderWinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comp5703traderwin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp5703TraderWinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
