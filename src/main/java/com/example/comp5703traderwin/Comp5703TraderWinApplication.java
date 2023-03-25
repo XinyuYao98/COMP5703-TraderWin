ff72fa9976885e1176de62c15aa1761ef93769a7
Xinyu yao <xyao6818@uni.sydney.edu.au>
2023-03-25 19:09:46 +0800

    the second test

--- src/main/java/com/example/comp5703traderwin/Comp5703TraderWinApplication.java
+++ src/main/java/com/example/comp5703traderwin/Comp5703TraderWinApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Comp5703TraderWinApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Comp5703TraderWinApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Comp5703TraderWinApplication.class, args);
+    }
 
 }
